6a33a68c86724e840b2e0971db5d639354f79042
Hakan Kayra Doğan <hkayrad@gmail.com>
2024-11-18 05:01:06 +0300

    feat: finish header design
    Signed-off-by: Hakan Kayra Doğan <hkayrad@gmail.com>

---
+++ components/header.php
@@ -0,0 +1,60 @@
+<link rel="stylesheet" href="./index.css">
+<link rel="stylesheet" href="./styles/header.css">
+
+<div class="header-wrapper">
+    <div class="header">
+        <div class="logo-container">
+            <a href="/">
+                <img src="./assets/brand/logo.svg" alt="">
+            </a>
+            <p><span>Premium'u</span> keşfet</p>
+        </div>
+
+        <div class="header-functions">
+            <div class="links">
+                <a href="">Siparişlerim</a>
+                <a href="">Süper Fiyat, Süper Teklif</a>
+                <a href="">Yurt Dışından</a>
+                <a href="">Kampanyalar</a>
+                <a href=""><span>Girişimci Kadınlar</span></a>
+                <a href="">Müşteri Hizmetleri</a>
+                <a href=""><span>Hepsiburada Premium</span></a>
+                <a href="">Hepsiburada'da Satıcı Ol</a>
+            </div>
+            <div class="other">
+                <div class="search">
+                    <img id="accent" src="./assets/brand/accent.svg" alt="">
+                    <div class="search-icon-wrapper">
+                        <img src="./assets/icons/search.svg">
+                    </div>
+                    <input type="text" placeholder="Ürün, kategori veya marka ara" />
+                </div>
+                <div class="location">
+                    <img id="location-icon" src="./assets/icons/location.svg" alt="">
+                    <div class="outer-wrapper">
+                        <p id="location-button-title">Konum</p>
+                        <div class="inner-wrapper">
+                            <p id="location-button">Ankara</p>
+                            <img id="chevron" src="./assets/icons/chevron_down.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="my-account">
+                    <img id="account-icon" src="./assets/icons/account.svg" alt="">
+                    <div class="outer-wrapper">
+                        <p id="location-button-title">Hesabım</p>
+                        <div class="inner-wrapper">
+                            <p id="location-button">Giriş Yap</p>
+                        </div>
+                    </div>
+                    <img id="chevron" src="./assets/icons/chevron_account.svg" alt="">
+                </div>
+                <a href="./pages/cart.php" class="cart">
+                    <img id="cart-icon" src="./assets/icons/cart.svg" alt="">
+                    <p id="cart-text">Sepetim</p>
+                </a>
+            </div>
+        </div>
+    </div>
+    <img id="top-accent" src="./assets/brand/accent.svg" alt="">
+</div>
