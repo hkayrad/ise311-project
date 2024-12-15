24e1c02446ad970ea1dd66729f6c59c0b5f5d5dd
Mert Kavilcioğlu <mertkavilci@gmail.com>
2024-12-15 13:02:09 +0300

    Added Header

--- pages/cart.php
+++ pages/cart.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cart</title>
+    <link rel="shortcut icon" href="../assets/brand/favicon.ico" type="image/x-icon">
+    <title>Sepetim</title>
 </head>
+
 <body>
+    <div class="header">
+    <p style="padding: 10px;">
+    <a href="/">
+        <img src="../assets/brand/logo.svg" alt="" style="margin: 0px;">
+        </a>
+        <br><span>Premium'u</span> keşfet
+        <span style="display: inline-block; width: 200px;"></span>   
+        <a href="">Kategoriler</a>
+        <input type="text" placeholder="Ürün, kategori veya marka ara" style="width: 1000px; height: 30px;  border-radius: 100px ;" />
+        <a href="">Siparişlerim</a>
+        <a href="">Profil</a>
+    </div>
+
     
 </body>
 </html>
