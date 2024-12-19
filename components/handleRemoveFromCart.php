b0b200c2f2ea142821db1651c411f026a97f7d5a
Hakan Kayra Doğan <hkayrad@gmail.com>
2024-12-19 23:56:47 +0300

    feat: finish cart
    Signed-off-by: Hakan Kayra Doğan <hkayrad@gmail.com>

--- components/handleRemoveFromCart.php
+++ components/handleRemoveFromCart.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['user_id'];
     $productId = $_POST['product_id'];
 
-    $query = "DELETE FROM cart WHERE user_id = $userId AND product_id = $productId";
+    $query = "DELETE FROM cart WHERE user_id = $userId AND product_id = $productId LIMIT 1";
     $result = mysqli_query($conn, $query);
 
     if (!$result) {
